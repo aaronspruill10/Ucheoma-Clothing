1d13ed75befe428d03f9910c8e00caa7b1413e62
Aaron Spruill <aaronspruill10@gmail.com>
2020-07-11 19:00:52 -0400

    July 11th 2020 7:00pm

--- server.js
+++ server.js
@@ -1,6 +1,56 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const stripe = require('stripe')('sk_test_51GzB5KCxnDLbnJ4akaFzJezJXzGkEQLywLr4VQ8FeghJnq6un5rpRtrGRwfIrDYYegigVdaB3pDVZFeUpHtRNJRp00KkR0ijJj');
+
+const session = await stripe.checkout.sessions.create({
+  payment_method_types: ['card'],
+  line_items: [{
+    price_data: {
+      currency: 'usd',
+      product_data: {
+        name: 'T-shirt',
+      },
+      unit_amount: 490,
+    },
+    quantity: 1,
+    {
+    price_data: {
+        currency: 'usd',
+        product_data: {
+          name: 'T-shirt',
+        },
+        unit_amount: 490,
+      },
+      quantity: 1,
+    }
+  }],
+
+  mode: 'payment',
+  success_url: 'https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
+  cancel_url: 'https://example.com/cancel',
+});
+
+
+
 const express = require('express')
 const app = express()
+const fs = require('fs')
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+
+const express = require('express');
+const app = express();
+
+app.get('/id', async (req, res) => {
+  const session = // ... Fetch or create the Checkout Session
+  res.json({session_id: session.id});
+});
+
+app.listen(3000, () => {
+  console.log('Running on port 3000');
+});
+
+
+
 app.listen(3000)
--- store.js
+++ store.js
@@ -3,20 +3,12 @@ if (document.readyState == 'loading') {
 } else {
     ready()
 }
-
-
 function ready() {
     var removeCartItemButtons = document.getElementsByClassName('btn-danger')
     for (var i = 0; i < removeCartItemButtons.length; i++) {
         var button = removeCartItemButtons[i]
         button.addEventListener('click', removeCartItem)
     }
-
-  //  var quantityInputs = document.getElementsByClassName('cart-quantity-input')
-  //  for (var i = 0; i < quantityInputs.length; i++) {
-  //      var input = quantityInputs[i]
-  //      input.addEventListener('change', quantityChanged)
-  //  }
 
     var addToCartButtons = document.getElementsByClassName('shop-item-button')
     for (var i = 0; i < addToCartButtons.length; i++) {
@@ -28,13 +20,6 @@ function ready() {
 
 }
 
-
-
-function myFunction() {
-
-
-
-}
 function hideOnClick(){
   document.getElementById('cartID').style.display = 'none';
 }
@@ -43,15 +28,8 @@ function showOnClick(){
 }
 
 
-
-
 function purchaseClicked() {
-    alert('Thank you for your purchase')
-    var cartItems = document.getElementsByClassName('cart-items')[0]
-    while (cartItems.hasChildNodes()) {
-        cartItems.removeChild(cartItems.firstChild)
-    }
-    updateCartTotal()
+
 }
 function removeCartItem(event) {
     var buttonClicked = event.target
